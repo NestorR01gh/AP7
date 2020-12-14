98f69964bdc2db4c667acdb8b49519eec74bf0fd
NestorR01 <neroaz@floridauniversitaria.es>
2020-12-14 17:09:31 +0100

    ej1

---
+++ ej1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
